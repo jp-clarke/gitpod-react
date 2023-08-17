a1c1524bc5735fc4d2ee2d477471461791eb976d
John Clarke <jp_clarke@outlook.com>
2023-08-17 09:26:10 +0000

    Props lesson

---
+++ my-app/src/components/FunctionalGreeting.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// function FunctionalGreeting() {
+//     return <h1>Hello from React!</h1>
+// }
+
+let FunctionalGreeting = () => <h1>Hello from React!</h1>
+
+export default FunctionalGreeting;
