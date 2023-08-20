f583da0ee0b7236693d90352f1a7307aaf692b30
John Clarke <jp_clarke@outlook.com>
2023-08-20 07:46:21 +0000

    Class components & updating the state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+// import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreetingWithProps greeting="nice to meet you!" name="Mike" age="32" />
+      {/* <FunctionalGreetingWithProps greeting="nice to meet you!" name="Mike" age="32" /> */}
+      <StatefulGreetingWithPrevState />
     </div>
   );
 }
